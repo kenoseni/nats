ad63e08077c98c504e8867155300419033a901c5
Olusola Oseni <kenolusola@gmail.com>
2022-03-12 12:18:45 +0100

    modify publisher base class

--- src/events/base-publisher.ts
+++ src/events/base-publisher.ts
@@ -11,9 +11,15 @@ export abstract class Publisher<T extends Event> {
 
   constructor(private readonly client: Stan) {}
 
-  publish(data: T["data"]) {
-    this.client.publish(this.channel, JSON.stringify(data), () => {
-      console.log("Event published");
+  publish(data: T["data"]): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.client.publish(this.channel, JSON.stringify(data), (err) => {
+        if (err) {
+          return reject(err);
+        }
+        console.log("Event published to channel", this.channel);
+        resolve();
+      });
     });
   }
 }
--- src/publisher.ts
+++ src/publisher.ts
@@ -8,14 +8,18 @@ const client = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
-client.on("connect", () => {
+client.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
   const publisher = new TicketCreatedPublisher(client);
 
-  publisher.publish({
-    id: "123",
-    title: "concert",
-    price: 20,
-  });
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "concert",
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
